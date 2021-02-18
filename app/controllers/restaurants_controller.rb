3e08af58f0d9aecd62cca6a0ad92f4bd4767c739
Nathaniel Santosa <nathaniel.santosa@gmail.com>
2021-02-18 13:53:35 +0800

    Done until restaurant controller

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,29 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @new_restaurant = Restaurant.new
+  end
+
+  def create
+    @new_restaurant = Restaurant.new(restaurant_params)
+
+    if @new_restaurant.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: %w[belgian japanese italian french chinese] }
+
+  has_many :reviews, dependent: :destroy
+end
